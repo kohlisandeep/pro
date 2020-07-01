66c0c24d6a1054f494685b4b685e225d12efc710
kohlisandeep <64226794+kohlisandeep@users.noreply.github.com>
2020-07-01 18:43:30 +0530

    Delete javaproject.java

--- javaproject.java
+++
@@ -1,9 +0,0 @@
-public class sandeep{
-public static void main(String[] arge)
-{
-
-System.out.println("its my first java project");
- its my new branch
-  and this is my first new branch also
-
-}}
